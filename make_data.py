934d277005e3bb41ecd8d80608627bf4fab0cfbc
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-11-14 14:23:45 -0500

    addition subtraction

--- make_data.py
+++ make_data.py
@@ -38,3 +38,37 @@ def division_dataset(number_type: str):
             src_dict[INPUT].append(f"{a} / {b} =")
             answer=a/b
             src_dict[OUTPUT].append(f"{answer}")
+
+def addition_dataset(number_type:str):
+    if number_type not in NUMBER_TYPE_LIST:
+        raise Exception("invalid number type")
+    src_dict={
+        INPUT:[],
+        OUTPUT:[]
+    }
+    for a in range(0,big_num):
+        for b in range(0,big_num):
+            if number_type==DECIMAL:
+                a=float(a)/big_num
+                b=float(b)/big_num
+            src_dict[INPUT].append(f"{a} + {b} =")
+            answer=a+b
+            src_dict[OUTPUT].append(f"{answer}")
+    return Dataset.from_dict(src_dict)
+
+def subtraction_dataset(number_type:str):
+    if number_type not in NUMBER_TYPE_LIST:
+        raise Exception("invalid number type")
+    src_dict={
+        INPUT:[],
+        OUTPUT:[]
+    }
+    for a in range(0,big_num):
+        for b in range(0,a):
+            if number_type==DECIMAL:
+                a=float(a)/big_num
+                b=float(b)/big_num
+            src_dict[INPUT].append(f"{a} - {b} =")
+            answer=a-b
+            src_dict[OUTPUT].append(f"{answer}")
+    return Dataset.from_dict(src_dict)
