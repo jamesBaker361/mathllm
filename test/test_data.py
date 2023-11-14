ff4444df7be1aba50dce46cf7566531664bd5c02
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2023-11-14 12:21:58 -0500

    data test

---
+++ test/test_data.py
@@ -0,0 +1,9 @@
+import unittest
+
+class DataLoadTest(unittest.TestCase):
+    def test_get_dataset(self):
+        '''
+        when given the tasks (multiplication, division, etc) and a number types (whole, decimal)
+        returns the train, test datasets
+        '''
+        return
