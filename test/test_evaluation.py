f40542dadc4e2d098fce2f04821672b89f500673
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2023-11-14 12:40:57 -0500

    nit

--- test/test_evaluation.py
+++ test/test_evaluation.py
@@ -4,7 +4,7 @@ class EvalTest(unittest.TestCase):
     def test_evaluate_wandb(self):
         '''
         when given the test dataset
-        for each model type (normal, peft, rl) loads model
+        and model type (normal, peft, rl) loads model
         and uploads metrics to wandb
         '''
         return
