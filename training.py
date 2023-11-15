cfdc8b9866e5eaf1d3409462d9013ee89c4d25c1
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2023-11-14 20:46:34 -0500

    training loop

--- training.py
+++ training.py
@@ -1,34 +1,20 @@
-from peft import LoraConfig, TaskType
-from datasets import Dataset,load_dataset,concatenate_datasets
-from string_globals import *
-import re
+from peft import LoraConfig, TaskType, get_peft_model
+from transformers import pipeline, set_seed, GPT2Tokenizer, GPT2Model,AutoTokenizer, AutoConfig, AutoModelForCausalLM,TrainingArguments
+from trl import SFTTrainer
+from datasets import Dataset
+import time
+from trl import PPOTrainer, PPOConfig, AutoModelForCausalLMWithValueHead, create_reference_model
+from trl.core import respond_to_batch
+import wandb
+import os
+from utils import download_datasets, reward_function
 
-SEED=1234
+os.environ["WANDB_API_KEY"]="004792fd620af032a735920a6cd036486b182519"
+os.environ["WANDB_NOTEBOOK_NAME"]="math-notebook"
+os.environ["WANDB_PROJECT"]="math-llm"
 
-def reward_function(decoded_response:str, answer:float, mae:bool=True)->float:
-    nums=re.findall(r'\d+\.\d+|\d+', decoded_response)
-    if len(nums)==0:
-        return -1000
-    guess=float(nums[0])
-    if mae:
-        return abs(guess-answer)
-    else:
-        return (guess-answer)**2
-    
-def download_datasets(task_list, number_type_list):
-    src_dict={
-        TEXT:[],
-        INPUT:[],
-        OUTPUT:[]
-    }
-    big_train=Dataset.from_dict(src_dict)
-    big_test=Dataset.from_dict(src_dict)
-    for task in task_list:
-        for number_type in number_type_list:
-            hf_dataset=load_dataset(f"jlbaker361/{task}_{number_type}")
-            split_dict=hf_dataset.train_test_split(.10,seed=SEED)
-            train=split_dict["train"]
-            test=split_dict["test"]
-            big_train=concatenate_datasets([big_train, train])
-            big_test=concatenate_datasets([big_test, test])
-    return big_train, big_test
+def training_loop(training_type_list:list[str],
+                  task_list:list[str],
+                  number_type_list:list[str]):
+    return
+
