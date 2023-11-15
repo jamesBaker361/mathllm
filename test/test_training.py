af4851cf8fef84d29b8420b5f2c78d55f4455128
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2023-11-14 21:18:47 -0500

    made test case

--- test/test_training.py
+++ test/test_training.py
@@ -1,11 +1,25 @@
 import unittest
+import os
+import sys
+sys.path.append(os.getcwd())
+from training import *
 
 class TrainingTest(unittest.TestCase):
-    def test_train_wandb(self):
+    def test_train_wandb_dummy(self):
         '''
-        when given the train dataset
+        when given the train dataset parameters
         and n epochs
-        and model type (normal, peft, rl) trains model
+        and model type =ft trains model
         and uploads metrics to wandb
         '''
-        return
+        for training_type_list in [[FT]]:
+            with self.subTest(training_type_list=training_type_list):
+                for task_list in [["dummy"]]:
+                    with self.subTest(task_list=task_list):
+                        for number_type_list in [[WHOLE], [WHOLE, DECIMAL], [DECIMAL]]:
+                            with self.subTest(number_type_list=number_type_list):
+                                training_loop(1, training_type_list, task_list, number_type_list)
+
+if __name__=='__main__':
+    test_case=TrainingTest()
+    test_case.test_train_wandb_dummy()
--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 from peft import LoraConfig, TaskType, get_peft_model
-from transformers import pipeline, set_seed, GPT2Tokenizer, GPT2Model,AutoTokenizer, AutoConfig, AutoModelForCausalLM,TrainingArguments
+from transformers import pipeline, set_seed, AutoTokenizer, AutoModelForCausalLM,TrainingArguments
 from trl import SFTTrainer
+from string_globals import *
 from datasets import Dataset
 import time
 from trl import PPOTrainer, PPOConfig, AutoModelForCausalLMWithValueHead, create_reference_model
@@ -13,8 +14,50 @@ os.environ["WANDB_API_KEY"]="004792fd620af032a735920a6cd036486b182519"
 os.environ["WANDB_NOTEBOOK_NAME"]="math-notebook"
 os.environ["WANDB_PROJECT"]="math-llm"
 
-def training_loop(training_type_list:list[str],
+def training_loop(epochs:int,
+                  training_type_list:list[str],
                   task_list:list[str],
                   number_type_list:list[str]):
-    return
+    peft_config = LoraConfig(task_type=TaskType.CAUSAL_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1)
+    for training_type in training_type_list:
+        task='+'.join(task_list)
+        number_type='+'.join(number_type)
+        run_name=f"{training_type}_{task}_{number_type}"
+        model=AutoModelForCausalLM.from_pretrained('gpt2')
+        model = get_peft_model(model, peft_config)
+        tokenizer = AutoTokenizer.from_pretrained('gpt2')
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
+        if training_type==FT:
+            ft_epochs=epochs
+            rl_epochs=0
+        elif training_type == RL:
+            ft_epochs=0
+            rl_epochs=epochs
+        elif training_type == MIXED:
+            ft_epochs=epochs//2
+            rl_epochs=epochs//2
+
+        args = TrainingArguments(
+            output_dir="./output",
+            num_train_epochs=ft_epochs,
+            # other args and kwargs here
+            run_name=run_name,
+            report_to="wandb",  # enable logging to W&B
+            logging_steps=1  # how often to log to W&B
+        )
+
+        train_dataset,_=download_datasets(task_list, number_type_list)
+
+        trainer = SFTTrainer(
+            model,
+            train_dataset=train_dataset,
+            dataset_text_field="text",
+            max_seq_length=128,
+            args=args
+        )
+
+        trainer.train()
+
+
 
