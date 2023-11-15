c1f0c8a0c0098828e54c454581631c7413290ebc
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-11-15 11:17:45 -0500

    run name

--- training.py
+++ training.py
@@ -3,13 +3,13 @@ from transformers import AutoTokenizer, AutoModelForCausalLM,TrainingArguments
 from trl import SFTTrainer
 from string_globals import *
 from datasets import Dataset
-import time
+import torch
 from trl import PPOTrainer, PPOConfig, AutoModelForCausalLMWithValueHead, create_reference_model
 from trl.core import respond_to_batch
 import numpy as np
 import wandb
 import os
-from utils import download_datasets, reward_function
+from utils import download_datasets, reward_function, get_run_name
 
 os.environ["WANDB_API_KEY"]="004792fd620af032a735920a6cd036486b182519"
 os.environ["WANDB_NOTEBOOK_NAME"]="math-notebook"
@@ -21,9 +21,7 @@ def training_loop(epochs:int,
                   number_type_list:list[str]):
     peft_config = LoraConfig(task_type=TaskType.CAUSAL_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1)
     for training_type in training_type_list:
-        task='+'.join(task_list)
-        number_type='+'.join(number_type_list)
-        run_name=f"{training_type}_{task}_{number_type}"
+        run_name=get_run_name(training_type, task_list, number_type_list)
         model=AutoModelForCausalLM.from_pretrained('gpt2')
         model = get_peft_model(model, peft_config)
         tokenizer = AutoTokenizer.from_pretrained('gpt2')
@@ -38,6 +36,17 @@ def training_loop(epochs:int,
         elif training_type == MIXED:
             ft_epochs=epochs//2
             rl_epochs=epochs//2
+        os.environ["WANDB_PROJECT"]=run_name
+        batch_size=8
+        run = wandb.init(
+            # Set the project where this run will be logged
+            project=run_name,
+            # Track hyperparameters and run metadata
+            config={
+                "rl_epochs": rl_epochs,
+                "ft_epochs":ft_epochs,
+                "rl_batch_size":batch_size
+            })
 
         args = TrainingArguments(
             output_dir="./output",
@@ -57,16 +66,34 @@ def training_loop(epochs:int,
             max_seq_length=128,
             args=args
         )
+        if ft_epochs>0:
+            trainer.train()
 
-        trainer.train()
+        ppo_config = PPOConfig(
+            batch_size=batch_size,
+        )
+        model_2=AutoModelForCausalLMWithValueHead.from_pretrained(get_peft_model(trainer.model, peft_config))
+        model_ref = create_reference_model(model_2)
+        ppo_trainer = PPOTrainer(ppo_config, model_2, model_ref, tokenizer)
 
-        batch_size=8
         batched_dataset=[t for t in train_dataset]
         batched_dataset=batched_dataset[:len(batched_dataset)-  (len(batched_dataset) %batch_size)]
         batched_dataset=np.reshape(batched_dataset, (len(batched_dataset)//batch_size, batch_size))
-        for e in range(ft_epochs, rl_epochs):
+        for e in range(ft_epochs,ft_epochs+ rl_epochs):
+            mean_scores=[]
             for batch in batched_dataset:
                 batch={key: [i[key] for i in batch] for key in batch[0]}
+                query_tensor = torch.cat([tokenizer.encode(q, return_tensors="pt") for q in batch[INPUT]])
+                response_tensor  = respond_to_batch(model_2, query_tensor)
+                reward = [torch.tensor(reward_function(tokenizer.decode(response),answer)) for response, answer in zip(response_tensor, batch[OUTPUT])]
+                train_stats = ppo_trainer.step([t for t in query_tensor], [t for t in response_tensor], reward)
+                mean_scores.append(train_stats['ppo/mean_scores'])
+            wandb.log({"ppo/mean_scores":np.mean(mean_scores)})
+
+        wandb.finish()
+        model_2.push_to_hub(run_name)
+
+        
                 
 
 
