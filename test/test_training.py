78d14a24bf740e3964d158d9400fe88ebd1f7665
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-11-15 10:22:12 -0500

    multiple trainings

--- test/test_training.py
+++ test/test_training.py
@@ -15,13 +15,13 @@ class TrainingTest(unittest.TestCase):
         and model type =ft trains model
         and uploads metrics to wandb
         '''
-        for training_type_list in [[FT]]:
+        for training_type_list in [[FT],[RL], [MIXED]]:
             with self.subTest(training_type_list=training_type_list):
                 for task_list in [["dumb"]]:
                     with self.subTest(task_list=task_list):
-                        for number_type_list in [[WHOLE], [WHOLE, DECIMAL], [DECIMAL]]:
+                        for number_type_list in [[WHOLE], [DECIMAL]]:
                             with self.subTest(number_type_list=number_type_list):
-                                training_loop(1, training_type_list, task_list, number_type_list)
+                                training_loop(2, training_type_list, task_list, number_type_list)
 
 if __name__=='__main__':
     test_case=TrainingTest()
